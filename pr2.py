7fc17f77770f109e06ff40df497c259ad6b5bb99
dskgit2627 <25dasikas@rbunagpur.in>
2026-02-05 22:54:51 +0530

    Create pr2.py
    This is a code that has 4 functions (addition, subtraction, multiplication and division). All 4 functions are called at the end of the code with values to demonstrate.

---
+++ pr2.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    s=a+b
+    print(s)
+    
+def sub(a,b):
+    s=a-b
+    print(s)
+    
+def mul(a,b):
+    s=a*b
+    print(s)
+    
+def div(a,b):
+    s=a/b
+    print(s)
+    
+add(3,4)
+sub(4,3)
+mul(3,4)
+div(6,3)
